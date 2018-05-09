eb1a20ce1cd1cd4186d2023419520905c599bb7b
wdmomoxx <540825306@qq.com>
2018-05-09 11:57:34 +0800

    Rename autoinstall.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+clear
+
+#    GPIO GameBoy keypad Installing by wdmomo
+
+echo  "       \033[0;32m█              █\033[0m     "
+echo  "  \033[0;33m█\033[0m     \033[0;32m█            █\033[0m     \033[0;33m█"
+echo  "\033[0;33m  █  ████████████████████  █"
+echo  "\033[0;33m  ███████\033[1;37m██\033[0;33m████████\033[1;37m██\033[0;33m███████"
+echo  "\033[0;33m  ██████████████████████████"
+echo  "\033[0;33m    ██████████████████████ "
+echo  "\033[0;33m     ████████████████████  "
+echo  "\033[0;33m        █             █   "
+echo  "\033[0;33m       █               █  "
+echo ""
+echo ""
+echo "*****************************************************************"
+echo "*                                                               *"
+echo "*           Installing GPIO GameBoy keypad by wdmomo            *"
+echo "*                                                               *"
+echo "*****************************************************************"
+sleep 3
+cd ~
+git clone git://git.drogon.net/wiringPi
+cd ~/wiringPi
+./build
+cd ~
+git clone https://github.com/wdmomoxx/Adafruit-Retrogame.git
+cd Adafruit-Retrogame
+make
+sudo install retrogame /usr/local/bin/retrogame
+sudo sed -i 's/retrogame\ &//g' /etc/rc.local
+sudo sed -i '/^exit\ 0/i\retrogame\ &' /etc/rc.local
+sudo sed -i '1i/SUBSYSTEM=="input",\ ATTRS{name}=="retrogame",\ ENV{ID_INPUT_KEYBOARD}="1"' /etc/udev/rules.d/10-retrogame.rules
+cd ~
+sudo rm -rf wiringPi/
+sudo rm -rf Adafruit-Retrogame/
+sudo reboot
